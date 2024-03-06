b2cc0e5e00d255e9c39e91fc8c2d298a701aa9f5
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-06 19:14:52 +0530

    Update CodeEditor styles and add Monaco Editor

--- app/components/CodeEditor/index.tsx
+++ app/components/CodeEditor/index.tsx
@@ -1,8 +1,33 @@
+"use client";
 import React from "react";
 import styles from "./CodeEditor.module.css";
+import Editor from "@monaco-editor/react";
+import Ajv from "ajv";
+const ajv = new Ajv({ allErrors: true });
 
 function CodeEditor() {
-	return <div className={styles.main}>CodeEditor</div>;
+	const [code, setCode] = React.useState<string | undefined>("");
+	return (
+		<div className={styles.main}>
+			<Editor
+				className={styles.editor}
+				height="70vh"
+				defaultLanguage="json"
+				defaultValue="{}"
+				value={code}
+				onChange={(value) => {
+					setCode(value);
+					try {
+						const schema = JSON.parse(value as string);
+						ajv.compile(schema);
+					} catch (error) {
+						console.error(error);
+					}
+				}}
+				theme=""
+			/>
+		</div>
+	);
 }
 
 export default CodeEditor;
