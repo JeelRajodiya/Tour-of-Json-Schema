886739b39ff543ce1a5e6b2d7c3fc715542a60c3
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-06 22:18:52 +0530

    Update Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,5 @@ import Instructions from "./components/Instructions";
 import styles from "./styles/page.module.css";
 
 export default async function Home() {
-	return (
-		<div className={styles.main}>
-			<Instructions />
-			<CodeEditor />
-		</div>
-	);
+    return <div className={styles.main}>Home</div>;
 }
