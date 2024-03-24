f9f51303a575e5a133ef95f75aed9b017e2e9a2d
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-24 11:10:59 +0530

    Add ReactGA for Google Analytics tracking

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,15 @@
 "use-client";
-
+import ReactGA from "react-ga4";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+// import { GoogleAnalytics } from "nextjs-google-analytics";
 
-export const Providers = ({ children }: { children: React.ReactNode }) => (
-    <>
-        <GoogleAnalytics gaMeasurementId="G-DTPB1R139Y" />
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </>
-);
+export function Providers({ children }: { children: React.ReactNode }) {
+    ReactGA.initialize("G-DTPB1R139Y");
+    return (
+        <>
+            {/* <GoogleAnalytics gaMeasurementId="G-DTPB1R139Y" /> */}
+            <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        </>
+    );
+}
