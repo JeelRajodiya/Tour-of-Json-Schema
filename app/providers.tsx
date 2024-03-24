5af0744dcebd3e2317e7737c11a226bf29e4817a
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-24 10:52:14 +0530

    Add Google Analytics tracking code

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,9 +2,11 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => (
     <>
+        <GoogleAnalytics gaMeasurementId="G-DTPB1R139Y" />
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
     </>
 );
