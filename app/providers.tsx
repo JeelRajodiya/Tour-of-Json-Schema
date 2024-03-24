1b5cf592b29612b6f95d2dbf995ebf1c0ac28bfa
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-24 11:17:27 +0530

    Fix ReactGA initialization in Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,16 @@
-"use-client";
+"use client";
 import ReactGA from "react-ga4";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
-// import { GoogleAnalytics } from "nextjs-google-analytics";
+import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    ReactGA.initialize("G-DTPB1R139Y");
+    useEffect(() => {
+        ReactGA.initialize("G-DTPB1R139Y");
+    }, []);
+
     return (
         <>
-            {/* <GoogleAnalytics gaMeasurementId="G-DTPB1R139Y" /> */}
             <ChakraProvider theme={theme}>{children}</ChakraProvider>
         </>
     );
