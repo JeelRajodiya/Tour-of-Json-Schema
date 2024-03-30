edcd5c931631aa58dd6632e56ab473b8426326e4
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-30 16:18:27 +0530

    Fix tsconfig.json and remove unnecessary file

---
+++ schemaForSchema.mjs
@@ -0,0 +1,51 @@
+
+import Ajv from "ajv/dist/2020.js";
+// @ts-ignore
+import betterAjvErrors from "better-ajv-errors";
+export function ajv(data, schema) {
+    const ajv = new Ajv({ allErrors: true, verbose: true }); // options can be passed, e.g. {allErrors: true}
+
+    const validate = ajv.compile(schema);
+    const valid = validate(data);
+    const errors = betterAjvErrors(schema, data, validate.errors);
+    return { valid, errors };
+
+}
+
+const data = [1, 2, 3];
+const userProvidedSchema = {
+    $schema: "https://json-schema.org/draft/2020-12/schema",
+	"type":"object",
+	"items":{
+		"type":"string"
+	
+	}
+};
+const validationSchema = {
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "type": "object",
+  "properties": {
+    "$schema": {
+      "type": "string",
+      "const": "https://json-schema.org/draft/2020-12/schema"
+    },
+    "type": {
+      "const": "array"
+    },
+    "items": {
+      "type": "object",
+      "properties": {
+        "type": {
+          "const": "number"
+        }
+      },
+      "required": ["type"],
+      "additionalProperties": false
+    }
+  },
+  "required": ["$schema", "type", "items"],
+  "additionalProperties": false
+};
+
+
+console.log(ajv(userProvidedSchema, validationSchema).errors);
