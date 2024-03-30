69468cf8cdc5aa3e36b044723b839547462af6e9
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-30 22:48:00 +0530

    Update padding and add back button to NavBar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -5,6 +5,8 @@ import { pageContext } from "@/lib/context";
 import { useContext } from "react";
 import jsonIcon from "@/public/icons/json-schema-blue.png";
 import Github from "@/public/icons/Github";
+import BackIcon from "@/public/icons/back.png";
+
 function Navbar() {
     const { pageName, setPageName } = useContext(pageContext);
 
@@ -26,6 +28,18 @@ function Navbar() {
                     />
                     <span className={styles.title}>Tour of JSON Schema</span>
                 </Link>
+                <div
+                    className={styles.backButton}
+                    onClick={() => {
+                        window.history.back();
+                    }}
+                >
+                    <img
+                        src={BackIcon.src}
+                        alt="back"
+                        style={{ width: "16px", height: "16px" }}
+                    />
+                </div>
                 {pageName}
             </div>
             <Link
