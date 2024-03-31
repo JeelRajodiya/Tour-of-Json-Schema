ef56d3b6a886bff22330e08953025cf9b53be5be
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-03-31 20:12:02 +0530

    refactor validation function

---
+++ app/step/2/validationFunction.ts
@@ -0,0 +1,45 @@
+import { ajv, hyperjumpValidate } from "@/lib/validators";
+
+export async function handleValidation(
+    setValidity: any,
+    setIsInvalid: any,
+    code: string | undefined
+) {
+    try {
+        const data = [1, 2, 3];
+        const schema = JSON.parse(code!);
+
+        const output = await hyperjumpValidate(data, schema);
+
+        const avjErrors = ajv(data, schema).errors;
+        if (output?.valid) {
+            setValidity(
+                "Great!\nLet's learn about $schema and specifications in the next step."
+            );
+
+            // Manual checking of types and items properties can be avoided,
+            // if we use validation Schema to validate the (user provided) schema itself.
+            // for more information view schemaForSchema.mjs file located at the root of the project
+
+            if (!schema.type) {
+                setIsInvalid(true);
+                setValidity(
+                    "Please specify the type of the data with the 'type' property."
+                );
+                return;
+            } else if (!schema.items) {
+                setIsInvalid(true);
+                setValidity("The schema should have an 'items' property.");
+                return;
+            }
+            setIsInvalid(false);
+        } else {
+            setValidity(avjErrors);
+
+            setIsInvalid(true);
+        }
+    } catch (e) {
+        setValidity(JSON.stringify((e as Error).message));
+        setIsInvalid(true);
+    }
+}
