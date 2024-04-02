bb72283b0823b61a32ad0ed2a20eb97d75aaece8
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-04-02 17:34:50 +0530

    Fix JSON Schema dialect validation

--- app/step/1/page.tsx
+++ app/step/1/page.tsx
@@ -17,14 +17,21 @@ async function handleValidation(
 ) {
     try {
         const schema = JSON.parse(code!);
-        if (schema.$schema !== "https://json-schema.org/draft/2020-12/schema") {
+        if (!schema.$schema) {
             setValidity(
                 "Unable to determine a dialect for the schema. The dialect can be declared in a number of ways, but the recommended way is to use the '$schema' keyword in your schema."
             );
             setIsInvalid(true);
             return;
+        } else if (
+            schema.$schema !== "https://json-schema.org/draft/2020-12/schema"
+        ) {
+            setValidity(
+                "Not a valid JSON Schema for draft 2020-12.\nPlease set $schema to https://json-schema.org/draft/2020-12/schema"
+            );
+            setIsInvalid(true);
+            return;
         }
-
         // I did not find any function in hyperjump that validates the schema without any data
         // So decided to use ajv for this step only
 
